b2694247fe4e2a6f30f74d94a919dbb5b6d11fa3
user815886724 <815886724@qq.com>
2018-09-18 17:40:59 +0800

    增加文字识别例子

--- src/test/java/com/face/test/Test.java
+++ src/test/java/com/face/test/Test.java
@@ -1,12 +1,31 @@
 package com.face.test;
 
+import com.baidu.aip.ocr.AipOcr;
 import com.face.utils.FileUtil;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
 
 public class Test {
-    public static void main(String[] args){
+    //设置APPID/AK/SK
+    public static final String APP_ID = "14223464";
+    public static final String API_KEY = "qYsu9owNq8HGEGrKBbg5mNSA";
+    public static final String SECRET_KEY = "ACFiKcgZGCTgtfS2kZcjB1InNjka7q8X";
+
+    public static void main(String[] args) throws JSONException{
+        AipOcr client = new AipOcr(APP_ID, API_KEY, SECRET_KEY);
+
+        // 可选：设置网络连接参数
+        client.setConnectionTimeoutInMillis(2000);
+        client.setSocketTimeoutInMillis(60000);
+
+        String path = "C:/Users/PabloWayne/Desktop/QQ图片20180918171609.png";
+        JSONObject res = client.basicGeneral(path, new HashMap<String, String>());
+        System.out.println(res.toString(2));
 
     }
 }
